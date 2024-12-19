e8a6bd6ee53451db46e13bed2af812edb9a9815e
Adrien Fauquembergue <adrien_fauque@hotmail.fr>
2024-12-19 16:38:51 +0100

    Fix bug Setting up registration

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -31,7 +31,7 @@ class RegistrationFormType extends AbstractType
             ->add('captcha', CaptchaType::class, [
                 'label' => 'Saisissez le code ci-dessous',
                 'reload' => true,
-                'as_url' => false,
+                'as_url' => true,
                 'invalid_message' => 'Le code est invalide.',
             ]);
     }
