50a8ce80bc701758365bf09b95993e145f62bb44
Adrien Fauquembergue <adrien_fauque@hotmail.fr>
2025-01-02 16:37:01 +0100

    test chaine CI

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,6 +12,8 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
  */
 class SecurityController extends AbstractController
 {
+
+
     /**
      * Affiche le formulaire de connexion et traite les erreurs d'authentification.
      *
