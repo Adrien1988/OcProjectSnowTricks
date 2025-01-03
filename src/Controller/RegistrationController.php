c758e86e81ef685c3dc2e0149d339dd1f6678781
Adrien Fauquembergue <adrien_fauque@hotmail.fr>
2025-01-03 14:59:50 +0100

    Removed old files for user registration management + used symfony maker bundle which generated the new files.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,177 +3,42 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\ActivationFormType;
 use App\Form\RegistrationFormType;
-use App\Service\MailerService;
+use App\Security\UserAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
-/**
- * Contrôleur pour la gestion de l'enregistrement et de l'activation des comptes utilisateurs.
- */
 class RegistrationController extends AbstractController
 {
-
-
-    /**
-     * Affiche et traite le formulaire d'enregistrement.
-     *
-     * @param Request                     $request        la
-     *                                                    requête
-     *                                                    HTTP
-     * @param EntityManagerInterface      $entityManager  le gestionnaire
-     *                                                    d'entités
-     * @param UserPasswordHasherInterface $passwordHasher le service de hachage de mots de passe
-     * @param MailerService               $mailerService  le service d'envoi d'emails
-     *
-     * @return Response la réponse HTTP
-     */
     #[Route('/register', name: 'app_register')]
-    public function register(
-        Request $request,
-        EntityManagerInterface $entityManager,
-        UserPasswordHasherInterface $passwordHasher,
-        MailerService $mailerService,
-    ): Response {
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
+    {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
-
         $form->handleRequest($request);
 
-        if (true === $form->isSubmitted() && true === $form->isValid()) {
-            // Vérification de l'unicité de l'email et du username.
-            $existingUser = $entityManager->getRepository(User::class)->findOneBy(['email' => $user->getEmail()]);
-            if (null !== $existingUser) {
-                $this->addFlash('error', 'Veuillez essayer de nouveau.');
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var string $plainPassword */
+            $plainPassword = $form->get('plainPassword')->getData();
 
-                return $this->redirectToRoute('app_register');
-            }
-
-            // Hashage du mot de passe.
-            $hashedPassword = $passwordHasher->hashPassword(
-                $user,
-                $form->get('password')->getData()
-            );
-            $user->setPassword($hashedPassword);
-
-            // Activation par email.
-            $user->setIsActive(false);
-
-            $token = bin2hex(random_bytes(32));
-            $user->setActivationToken($token);
-
-            $avatarMethod = $form->get('avatarMethod')->getData();
-
-            if ('url' === $avatarMethod) {
-                $avatarUrl = $form->get('avatarUrl')->getData();
-                if (null !== $avatarUrl) {
-                    $user->setAvatarUrl($avatarUrl);
-                }
-            }
-
-            if ('upload' === $avatarMethod) {
-                $avatarFile = $form->get('avatarFile')->getData();
-                if (null !== $avatarFile) {
-                    $newFilename = uniqid().'.'.$avatarFile->guessExtension();
-                    $avatarFile->move($this->getParameter('avatars_directory'), $newFilename);
-                    $user->setAvatarUrl('/uploads/avatars/'.$newFilename);
-                }
-
-                if ($avatarFile === false) {
-                    $this->addFlash('error', "Vous n'avez pas uploadé d'avatar !");
-
-                    return $this->redirectToRoute('app_register');
-                }
-            }
+            // encode the plain password
+            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
 
             $entityManager->persist($user);
             $entityManager->flush();
 
-            $mailerService->sendActivationEmail($user->getEmail(), $token);
+            // do anything else you need here, like send an email
 
-            $this->addFlash('success', 'Votre compte a été créé avec succès ! Veuillez vérifier votre email pour l’activer.');
-
-            return $this->redirectToRoute('app_login');
-        }// end if
-
-        return $this->render(
-            'registration/register.html.twig',
-            [
-                'registrationForm' => $form->createView(),
-            ]
-        );
-    }// end register()
-
-
-    /**
-     * Affiche et traite le formulaire d'activation de compte.
-     *
-     * @param string                      $token          le token d'activation
-     * @param Request                     $request        la requête
-     *                                                    HTTP
-     * @param EntityManagerInterface      $entityManager  le gestionnaire
-     *                                                    d'entités
-     * @param UserPasswordHasherInterface $passwordHasher le service de hachage de mots de passe
-     *
-     * @return Response la réponse HTTP
-     */
-    #[Route('/activate/{token}', name: 'app_activate_account', methods: ['GET', 'POST'])]
-    public function showActivationForm(
-        string $token,
-        Request $request,
-        EntityManagerInterface $entityManager,
-        UserPasswordHasherInterface $passwordHasher,
-    ): Response {
-        $user = $entityManager->getRepository(User::class)->findOneBy(['activationToken' => $token]);
-
-        if (null === $user) {
-            $this->addFlash('error', 'Token invalide.');
-
-            return $this->redirectToRoute('app_login');
+            return $security->login($user, UserAuthenticator::class, 'main');
         }
 
-        $form = $this->createForm(ActivationFormType::class);
-        $form->handleRequest($request);
-
-        if (true === $form->isSubmitted() && true === $form->isValid()) {
-            $data = $form->getData();
-            $email = $data['email'];
-            $enteredPassword = $data['password'];
-
-            if ($email !== $user->getEmail()) {
-                $this->addFlash('error', 'L’email ne correspond pas à celui associé au compte.');
-
-                return $this->redirectToRoute('app_activate_account', ['token' => $token]);
-            }
-
-            if (false === $passwordHasher->isPasswordValid($user, $enteredPassword)) {
-                $this->addFlash('error', 'Le mot de passe est incorrect.');
-
-                return $this->redirectToRoute('app_activate_account', ['token' => $token]);
-            }
-
-            $user->setIsActive(true);
-            $user->setActivationToken(null);
-            $entityManager->flush();
-
-            $this->addFlash('success', 'Votre compte est maintenant actif !');
-
-            return $this->redirectToRoute('app_login');
-        }// end if
-
-        return $this->render(
-            'registration/activate.html.twig',
-            [
-                'activationForm' => $form->createView(),
-                'token'          => $token,
-            ]
-        );
-    }// end showActivationForm()
-
-
-}// end class
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form,
+        ]);
+    }
+}
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,115 +3,53 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Gregwar\CaptchaBundle\Type\CaptchaType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
-/**
- * Formulaire d'inscription pour un nouvel utilisateur.
- */
 class RegistrationFormType extends AbstractType
 {
-
-
-    /**
-     * Construit le formulaire d'inscription.
-     *
-     * @param FormBuilderInterface $builder instance utilisée pour construire les champs du formulaire
-     * @param array                $options options supplémentaires pour le formulaire
-     *
-     * @return void
-     */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add(
-                'email',
-                EmailType::class,
-                [
-                    'label' => 'Adresse email',
-                ]
-            )
-            ->add(
-                'password',
-                RepeatedType::class,
-                [
-                    'type'            => PasswordType::class,
-                    'invalid_message' => 'Les mots de passe doivent correspondre.',
-                    'required'        => true,
-                    'first_options'   => ['label' => 'Mot de passe'],
-                    'second_options'  => ['label' => 'Confirmez le mot de passe'],
-                ]
-            )
-            ->add(
-                'avatarMethod',
-                ChoiceType::class,
-                [
-                    'choices' => [
-                        'Ajouter l\'avatar ultérieurement' => 'none',
-                        'URL'                              => 'url',
-                        'Uploader un fichier'              => 'upload',
-                    ],
-                    'expanded' => true,
-                    'multiple' => false,
-                    'mapped'   => false,
-                    'label'    => 'Comment fournir votre avatar ?',
-                ]
-            )
-            ->add(
-                'avatarUrl',
-                TextType::class,
-                [
-                    'label'    => 'URL de votre avatar',
-                    'required' => false,
-                    'mapped'   => false,
-                ]
-            )
-            ->add(
-                'avatarFile',
-                FileType::class,
-                [
-                    'required' => false,
-                    'label'    => 'Fichier avatar',
-                    'mapped'   => false,
-                ]
-            )
-            ->add(
-                'captcha',
-                CaptchaType::class,
-                [
-                    'label'           => 'Saisissez le code ci-dessous',
-                    'reload'          => true,
-                    'as_url'          => true,
-                    'invalid_message' => 'Le code est invalide.',
-                ]
-            );
-    }// end buildForm()
+            ->add('email')
+            ->add('agreeTerms', CheckboxType::class, [
+                                'mapped' => false,
+                'constraints'            => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped'      => false,
+                'attr'        => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min'        => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+    }
 
-
-    /**
-     * Configure les options par défaut du formulaire.
-     *
-     * @param OptionsResolver $resolver instance pour définir les options
-     *
-     * @return void
-     */
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults(
-            [
-                'data_class' => User::class,
-            ]
-        );
-    }// end configureOptions()
-
-
-}// end class
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
