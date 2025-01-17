2f94e84d08f130d05aae83d1117b73216163c3f5
Adrien1988 <80693365+Adrien1988@users.noreply.github.com>
2025-01-17 14:12:20 +0100

    14 figure 002 creating a figure (#46)
    * creation of the symfony base
    
    * modify the .env file and test BDD connection with doctrine:database:create.
    
    * install doctrine-fixtures-bundle and webpack.
    
    * Added the User entity and its repository.
    
    * Added the Comment entity and its repository.
    
    * creation of entities and repositories.
    
    * Preparing for password hashing.
    
    * mise en place système d'authentification
    
    * Merge branch 'main' of https://github.com/Adrien1988/OcProjectSnowTricks into 10-auth-001-setting-up-authentication
    
    * retrait stimulus, turbo et assetMapper
    
    * Setting up registration
    
    * Fix bug Setting up registration
    
    * Finalizing the user registration function.
    
    * Setting up CI file
    
    * test chaine CI
    
    * test chaine CI
    
    * chaine CI
    
    * correction of smell codes following the implementation of the CI chain.
    
    * modified php-cs-fixer and codeSniffer files to handle PSR standards.
    
    * correction of smell codes
    
    * modification of cs-fixer and codesniffer files + test on a controller.
    
    * test chaine CI
    
    * modification of cs-fixer file + test on controllers.
    
    * fixed remaining smels codes.
    
    * CI chain modification
    
    * New CI chain modification
    
    * CI chain push modification
    
    * CI chain push modification
    
    * fixed codacy smells for registrationController.
    
    * fixed codacy smells for registrationController.
    
    * delete old authentication management files + create new files with symfony maker bundle.
    
    * Removed old files for user registration management + used symfony maker bundle which generated the new files.
    
    * Fixed code smells for registrationController and registrationForm files.
    
    * replacing custom files with files generated by symfony + adding logic for managing registration in the controller and in the formType.
    
    * fix 2 issues Codacy
    
    * creation of the forgotten password management system.
    
    * creation of the forgotten password management system.
    
    * correction code smells system forgotten password reset.
    
    * creation of the HomeController and the view template with the symfony maker:bundle.
    
    * fix bootstrap with webpack + add method in FigureRepository + edit home/index.html.twig
    
    * refacto forgot and reset controller in securityController + evolution templates twig reset and forgot.
    
    * creating files for new trick issue.
    
    * Implementation of the code in the figure creation files.
    
    * BDD update
    
    * separation of adding videos and photos from creating a figure.
    
    * fix code style

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Figure;
+use App\Form\FigureType;
 use App\Repository\FigureRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -12,22 +16,41 @@ class HomeController extends AbstractController
 
 
     /**
-     * Affiche la liste des figures de snowboard.
+     * Affiche la liste des figures de snowboard et permet d'en créer une via une modale.
      *
-     * @param FigureRepository $figureRepository Le dépôt des figures
+     * @param FigureRepository       $figureRepository Le dépôt des figures
+     * @param Request                $request          La requête HTTP
+     * @param EntityManagerInterface $entityManager    Gestionnaire d'entités pour persister les données
      *
      * @return Response La réponse HTTP
      */
-    #[Route('/', name: 'app_home')]
-    public function index(FigureRepository $figureRepository): Response
+    #[Route('/', name: 'app_home', methods: ['GET', 'POST'])]
+    public function index(FigureRepository $figureRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
 
         $figures = $figureRepository->findAllWithImages();
 
+        // Création du formulaire pour ajouter une figure
+        $figure = new Figure();
+        $createFigureForm = $this->createForm(FigureType::class, $figure);
+        $createFigureForm->handleRequest($request);
+
+        // Gestion de la soumission du formulaire
+        if ($createFigureForm->isSubmitted() && $createFigureForm->isValid()) {
+            $entityManager->persist($figure);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'La figure a été créée avec succès.');
+
+            // Redirige pour éviter la resoumission du formulaire
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render(
             'home/index.html.twig',
             [
-                'figures' => $figures,
+                'figures'          => $figures,
+                'createFigureForm' => $createFigureForm->createView(),
             ]
         );
     }
---
+++ src/Form/FigureType.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Figure;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FigureType extends AbstractType
+{
+
+
+    /**
+     * Construit le formulaire pour une figure.
+     *
+     * Définit les champs du formulaire de création/modification d'une figure.
+     *
+     * @param FormBuilderInterface $builder constructeur du formulaire
+     * @param array                $options options du formulaire
+     *
+     * @return void
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add(
+                'name',
+                TextType::class,
+                [
+                    'label' => 'Nom de la figure',
+                    'attr'  => [
+                        'placeholder' => 'Entrez le nom de la figure',
+                    ],
+                ]
+            )
+            ->add(
+                'description',
+                TextareaType::class,
+                [
+                    'label' => 'Description',
+                    'attr'  => [
+                        'placeholder' => 'Décrivez la figure',
+                    ],
+                ]
+            )
+            ->add(
+                'figureGroup',
+                TextType::class,
+                [
+                    'label' => 'Groupe de la figure',
+                    'attr'  => [
+                        'placeholder' => 'Indiquez le groupe de la figure',
+                    ],
+                ]
+            );
+    }
+
+
+    /**
+     * Configure les options du formulaire.
+     *
+     * Associe le formulaire à l'entité Figure.
+     *
+     * @param OptionsResolver $resolver résolveur des options
+     *
+     * @return void
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => Figure::class,
+            ]
+        );
+    }
+
+
+}
